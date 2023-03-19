80a1573f86615b5fe135230cf764e5eea9688cce
kmi0817 <kmi0817@naver.com>
2023-03-20 01:27:40 +0900

    chore: :truck: optimize folder structure

--- app/app.js
+++ app/app.js
@@ -4,10 +4,10 @@ const express = require("express"); // express 모듈
 const app = express(); // express 실행하여 app 변수에 할당
 
 // Routing
-const home = require("../routers/home");
+const home = require("./src/routes/home");
 
 // app setting (View Engine Setting)
-app.set("views", "./views"); // 화면 뷰 관리할 파일이 저장될 폴더 이름이 2번째 파라미터
+app.set("views", "./src/views"); // 화면 뷰 관리할 파일이 저장될 폴더 이름이 2번째 파라미터
 app.set("view engine", "ejs"); // 뷰 엔진으로 ejs을 사용함
 
 app.use("/", home); // use: 미들웨어를 등록하는 메소드
