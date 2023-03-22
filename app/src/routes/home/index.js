98bb301adfe998fa8aedceecb22f4f60a5e92914
kmi0817 <kmi0817@naver.com>
2023-03-22 16:13:32 +0900

    feat: make login API and upgrade codes
    make an output, and then put hello, login in it (instead of hello, login). Also, make a process that reads req.body

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
-router.get("/", ctrl.hello);
-
-router.get("/login", ctrl.login);
+router.get("/", ctrl.output.hello);
+router.get("/login", ctrl.output.login);
+router.post("/login", ctrl.process.login);
 
 module.exports = router; // 외부 폴더에서 router를 사용할 수 있도록 외부로 내보냄
