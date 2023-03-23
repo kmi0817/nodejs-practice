4c2f273e83f59b2a04cd9290e952b1f008e03f49
kmi0817 <kmi0817@naver.com>
2023-03-23 17:15:45 +0900

    feat: make a register page (front)

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.hello);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 module.exports = router; // 외부 폴더에서 router를 사용할 수 있도록 외부로 내보냄
