28342218a769748c7746b7b7f9ce67cabd1381ab
kmi0817 <kmi0817@naver.com>
2023-03-23 23:37:01 +0900

    chore: fix wrong variable name

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -10,12 +10,12 @@ class User {
     async login() {
         const client = this.body;
         try {
-            const { id, psword } = await UserStorage.getUserInfo(client.id);
+            const { id, password } = await UserStorage.getUserInfo(client.id);
             // getUserInfo 메소드의 반환값이 Promise 객체이므로, await으로 해당 데이터를 다 받아오도록 설정함
             // await은 async 안에서만 동작 가능하므로, login 앞에 async
 
             if (id) {
-                if (id === client.id && psword === client.psword) {
+                if (id === client.id && password === client.password) {
                     return { success: true };
                 }
                 return { success: false, msg: "비밀번호가 틀렸습니다." };
