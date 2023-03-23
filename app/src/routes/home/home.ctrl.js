582c294044ddf122adba29e7d542156b4a52d6aa
kmi0817 <kmi0817@naver.com>
2023-03-23 16:07:44 +0900

    refactor: :recycle: make a response object
    It's clearer than before

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -19,20 +19,19 @@ const process = {
     login: (req, res) => {
         const id = req.body.id,
             password = req.body.password;
-            
+        
+        const response = {};
         if (users.id.includes(id)) {
             const idx = users.id.indexOf(id);
             if (users.password[idx] === password) {
-                return res.json({
-                    success: true,
-                });
+                response.success = true;
+                return res.json(response);
             }
         }
 
-        return res.json({
-            success: false,
-            msg: "로그인에 실패하셨습니다"
-        });
+        response.success = false;
+        response.msg = "로그인에 실패하셨습니다";
+        return res.json(response);
     }
 };
 
